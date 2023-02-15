0e994ccb4a19f73e95a4128b8ace1128d1eb769d
Capopato <107575282+Capopato@users.noreply.github.com>
2023-02-15 13:37:49 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,32 @@ customer.chooseToppings({ tomatoSauce: "yes", cheese: "yes", mozzarella: "yes", 
 customer.choosePizza({ choice: "Normal" });
 customer.calculatePrice();
 console.log(customer);
+//Prints out:
+/**
+ * Customer {
+  pizza: 'Normal',
+  toppings: [ 'TomatoSauce', 'Cheese', 'Mozzarella' ],
+  priceForToppings: 4,
+  priceForPizza: 6,
+  totalPrice: 10
+}
+ */
+
 
 const customer2 = new Customer();
 customer2.chooseToppings({ tomatoSauce: "yes", cheese: "yes", mozzarella: "yes", onion: "yes", tuna: "yes" });
 customer2.choosePizza({ choice: "Calzone" });
 customer2.calculatePrice();
 console.log(customer2);
+
+//Prints out: 
+/**
+ * Customer {
+  pizza: 'Calzone',
+  toppings: [ 'TomatoSauce', 'Cheese', 'Onion', 'Mozzarella', 'Tuna' ],
+  priceForToppings: 8,
+  priceForPizza: 7.5,
+  totalPrice: 15.5
+}
+ */
+
